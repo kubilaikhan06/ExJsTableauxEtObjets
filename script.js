26e51a22fd51d0cc6f02d2099fb33733db9e2cf3
kubilaikhan06 <tld3906@hotmail.com>
2018-07-02 15:51:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//tableau ordinaire par instanciation d'un Array
+var personne = new Array('nom', 'prenom');
+console.log(personne[0]);
+//tableau associatif
+var autrePersonne = new Array();
+autrePersonne['nom']= 'Marc';
+autrePersonne['prenom'] = 'machin';
+console.log(autrePersonne['prenom']);
+console.log(autrePersonne.nom);
+//déclaration d'un tableau littéral
+var encorePersonne = {"nom": "ploin", "prenom": "john"};
+console.log(encorePersonne['prenom']);
+console.log(encorePersonne.nom);
+
+$(document).ready(function(){
+    
+    //objet personne
+    function Personne(){
+        this.nom;
+        this.prenom;
+    };
+    var homme = new Personne();// instanciation d'une personne
+    homme.nom = 'Milord';
+    homme.prenom = 'Albert';
+    console.log(homme.nom);
+    document.getElementById('div1').innerHTML = encorePersonne.nom;
+    document.getElementById('para1').innerHTML = homme.prenom;
+    document.getElementById('nom').value = autrePersonne.prenom;
+    document.getElementById('prenom').value = homme.prenom;
+    //affiche une alert lors d'un click sur le bouton
+    document.getElementById('btnAffiche').onclick= function(){
+        alert(homme.nom);
+    };
+
+    //fonction anonyme auto-exécutable
+    (function(){
+        alert(encorePersonne.prenom);
+    })();
+});
